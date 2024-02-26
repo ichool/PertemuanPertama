86a3fb01110523b88935d4ff54e9d502f584803b
ichool <faisalaja2653@gmail.com>
2024-02-26 11:42:52 +0700

    masukan jejari

--- PertemuanPertama/PertemuanPertama.cpp
+++ PertemuanPertama/PertemuanPertama.cpp
@@ -6,8 +6,16 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    cout << "hello";
+   // begin
+        // numeric nJejari, nLuas
+        // display 'Masukan jejari= '
+        // accept nJejari
+        // compute nLuas = 3.14 * r * r
+        // display 'Luasnya= ' + nLuas
+   // end
+
+    double nJejari, nLuas;
+    cout << "masukan Jejari = ";
 
     system("pause");
 }
